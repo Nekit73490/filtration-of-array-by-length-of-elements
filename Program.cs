14d5321a3417631976305332b9382540e17afdce
nekit73490 <nekit73490@gmail.com>
2022-09-21 13:51:11 +0400

    added comments

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
-﻿int target_length = 3, a = 0;
+﻿// variables input
+int target_length = 3, a = 0;
 string[] input_array = new string[] {"hell", "3", "war", ":-(", "as expected"};
 string[] temp_array = new string[input_array.Length];
+
+//searching for elements with symbol length < 3
 for(int i = 0; i<input_array.Length; i=i+1){
     if (input_array[i].Length <= target_length)
     {
@@ -10,12 +13,12 @@ for(int i = 0; i<input_array.Length; i=i+1){
     
     }
 
-
+// getting rid of empty elements
 string[] pretty_output_array = new string[a];
 for(int i = 0; i<a; i=i+1)
     pretty_output_array[i] = temp_array[i];
 
-
+//printing the result
 for(int i = 0; i<a; i=i+1)
     System.Console.WriteLine(pretty_output_array[i]);
     
